ce3f60a5341ce52f64f2657b35a1337efe5d7e51
Andrey Mendonca <andrey.mmendonca@gmail.com>
2025-04-21 15:55:58 -0300

    Projeto site de questões

---
+++ estados_e_eventos/src/app/questoes/page.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import { QuestionItem } from "@/components/QuestionItem";
+import { Result } from "@/components/Result";
+import { questions } from "@/data/questions";
+import { useState } from "react"
+
+const page = () =>{
+    const [answers, setAnswers]= useState<number[]>([]);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showResult, setShowResult] = useState(false);
+    const title = 'Quiz de Culinária'
+
+    const loadNextQuestion = () =>{
+        if(questions[currentQuestion +1]){
+            setCurrentQuestion(currentQuestion+1)
+        }else{
+            setShowResult(true);
+        }
+    }
+
+    const handleAnswered = (answer: number) =>{
+        setAnswers([...answers, answer]);
+        loadNextQuestion();
+    }
+
+    const handleRestarButton = () =>{
+        setAnswers([]);
+        setCurrentQuestion(0);
+        setShowResult(false);
+    }
+
+    return (
+        <div className="w-full h-screen flex justify-center items-center bg-blue-600">
+            <div className="w-full max-w-xl rounded-md bg-white text-black shadow shadow-black">
+                <div className="p-5 font-bold text-2xl border-b border-gray-300">
+                    {title}
+                </div>
+                <div className="p-5">
+                    {!showResult &&
+                        <QuestionItem 
+                        question={questions[currentQuestion]}
+                        count={currentQuestion + 1}
+                        onAnswer={handleAnswered}
+                        />
+                    }
+                    {showResult &&
+                        <Result questions={questions} answers={answers}/>
+                    }
+                </div>
+                <div className="p-5 text-center border-t border-gray-300">
+                    {!showResult &&
+                        `${currentQuestion + 1} de ${questions.length} pergunta${questions.length !== 1 ? 's' : ''}`
+                    }
+                    {showResult &&
+                        <button className="px-3 py-2 rounded-md bg-blue-700 text-white cursor-pointer" onClick={handleRestarButton}>Reiniciar quiz</button>
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default page;
---
+++ estados_e_eventos/src/data/questions.ts
@@ -0,0 +1,24 @@
+import { Question } from "@/types/Question";
+
+export const questions : Question[] = [
+    {
+        question: 'Qual ingrediente não pode faltar?',
+        options:[
+            'Farina',
+            'Corante',
+            'Agua',
+            'Acucar'
+        ],
+        answer : 1
+    },
+    {
+        question: 'Qual ingrediente não pode faltar?',
+        options:[
+            'Farina',
+            'Corante',
+            'Agua',
+            'Acucar'
+        ],
+        answer : 2
+    }
+]
