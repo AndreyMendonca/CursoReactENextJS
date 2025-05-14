e7b9ff3f7bc0a762d3469329d71c68942ae6785a
Andrey Mendonça <97072306+AndreyMendonca@users.noreply.github.com>
2025-05-14 11:34:19 -0300

    Usando LocalStorage

--- reducer_e_context/src/context/PostContext.tsx
+++ reducer_e_context/src/context/PostContext.tsx
@@ -1,5 +1,7 @@
 import { postReducer } from "@/reducers/postReduces";
-import { createContext, ReactNode, useReducer, useState } from "react";
+import { createContext, ReactNode, useEffect, useReducer, useState } from "react";
+
+const STORAGE_KEY= 'postContextContent'
 
 type PostContextType = {
     posts: Post[];
@@ -10,8 +12,14 @@ type PostContextType = {
 export const PostContext = createContext<PostContextType | null>(null);
 
 export const PostProvider = ({children}:{children:ReactNode}) =>{
-    const [posts, dispatch] = useReducer(postReducer, []);
+    const [posts, dispatch] = useReducer(
+        postReducer, 
+        JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    );
 
+    useEffect(() =>{
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(posts))
+    },[posts]);
 
     const addPost = (title: string, body: string) => {
         dispatch({
