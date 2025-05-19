9744ec0f9da9b30775996b0e918763cbafb1b9f7
Andrey Mendonça <97072306+AndreyMendonca@users.noreply.github.com>
2025-05-19 18:30:22 -0300

    Aulas sobre tanStack Query

--- tanstackquery1/src/app/page.tsx
+++ tanstackquery1/src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client"
+import { usePost, usePosts } from "@/utils/queries";
+
 const Page = () =>{
+  const posts = usePosts();
+  const postItem = usePost(10);
+  
   return (
     <div className="container mx-auto">
-      oi
+      {posts.isLoading && 'Carregando...'}
+      {posts.data &&
+        <ul>
+          {posts.data.map(item => (
+            <li key={item.id}>{item.title}</li>
+          ))}
+        </ul>
+      }
     </div>
   )
 }
