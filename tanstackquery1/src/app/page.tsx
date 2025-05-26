cbd92670c67f492c46a745f9d762e2a1c636a678
Andrey Mendonça <97072306+AndreyMendonca@users.noreply.github.com>
2025-05-26 16:30:08 -0300

    Continuação aulas sobre TanStack Query

--- tanstackquery1/src/app/page.tsx
+++ tanstackquery1/src/app/page.tsx
@@ -1,13 +1,21 @@
 "use client"
 import { usePost, usePosts } from "@/utils/queries";
+import { useState } from "react";
 
 const Page = () =>{
-  const posts = usePosts();
+  const [canLoadPosts, setCanLoadPosts] = useState(false);
+  const posts = usePosts(canLoadPosts);
   const postItem = usePost(10);
   
+  const handleLoadPostsButton = () =>{
+    setCanLoadPosts(true);
+  }
+
   return (
     <div className="container mx-auto">
+      <button onClick={handleLoadPostsButton} className="border p-3 rounded-md m-3">Carregar posts</button>
       {posts.isLoading && 'Carregando...'}
+      {posts.isFetched && 'RECarregando...'}
       {posts.data &&
         <ul>
           {posts.data.map(item => (
--- tanstackquery1/src/utils/queries.ts
+++ tanstackquery1/src/utils/queries.ts
@@ -1,14 +1,21 @@
 import { useQuery } from "@tanstack/react-query"
 import { getPost, getPosts } from "./api"
 
-export const usePosts = () => {
+export const usePosts = (enabled?: boolean) => {
     const query = useQuery({
         queryKey: ['posts'], 
-        queryFn: getPosts
+        queryFn: getPosts,
+        enabled: enabled
     });
     return query
 }
 
+//staleTime = configurar o tempo que cada requisição será considerada  STALE, ou seja, que é um dado que precisa ser atualizado
+//staleTime: Infinity
+
+/* 
+    enable: true
+*/
 
 export const usePost = (id:number) => {
     const query = useQuery({
