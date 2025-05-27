11591ae4a17f853187ea4dbeec2e6ba26ea1549b
Andrey Mendonça <97072306+AndreyMendonca@users.noreply.github.com>
2025-05-27 17:30:58 -0300

    Criando side Effects em mutations

--- tanstackquery1/src/app/page.tsx
+++ tanstackquery1/src/app/page.tsx
@@ -19,7 +19,19 @@ const Page = () => {
   }
 
   const addMutation = useMutation({
-    mutationFn: addPost
+    mutationFn: addPost,
+    onMutate: (data) =>{
+      console.log('dados da mutation', data)
+    },
+    onError: (error, data, context) => {
+      console.log('pode adicionar a logica de quando der erro')
+    },
+    onSuccess: (retorno, data, context) => {
+      console.log('logica de sucesso')
+    },
+    onSettled: (retorno, erro, context) =>{
+      console.log('Independente se dar erro ou nao, ele sempre executará essa código')
+    }
   })
 
   const handleAddButton = () => {
@@ -35,6 +47,7 @@ const Page = () => {
       <button onClick={handleLoadPostsButton} className="border p-3 rounded-md m-3">Carregar posts</button>
       <button onClick={handleInvalidadePostButton} className="border p-3 rounded-md m-3">Invalidar posts</button>
       <button onClick={handleAddButton} className="border p-3 rounded-md m-3">Adicionar posts</button>
+      <p onClick={() => addMutation.reset()}>Status Mutation: {addMutation.status}</p>
       {posts.isLoading && 'Carregando...'}
       {posts.isFetched && 'RECarregando...'}
       {posts.data &&
