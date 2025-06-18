a0aeb66eddee1756232d8d7228f583e37abb8bd6
Andrey Mendonça <97072306+AndreyMendonca@users.noreply.github.com>
2025-06-18 16:23:17 -0300

    Criação do lado de inserção dos dados

--- calculadora-imc/src/app/page.tsx
+++ calculadora-imc/src/app/page.tsx
@@ -1,103 +1,53 @@
-import Image from "next/image";
+'use client'
+import Image from 'next/image';
+import { useState } from 'react';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+const Page = () => {
+    const [heightField, setHeightField] = useState<number>(0);
+    const [weightField, setWeightField] = useState<number>(0);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+    const handleCalculateButton = () =>{
+        if(heightField && weightField){
+
+        }else{
+            alert('Coloque todos os campos')
+        }
+    }
+
+    return (
+        <div>
+            <header>
+                <div className="max-w-4xl my-15 mx-auto">
+                    <Image src="/powered.png" alt="logo" width={150} height={150} />
+                </div>
+            </header>
+            <div className="max-w-4xl flex mx-auto justify-between">
+                <div className="flex-1 mr-10">
+                    <h1 className="text-4xl text-gray-700">Calcule o seu IMC</h1>
+                    <p className="text-xl text-gray-500 mb-10">O IMC (Índice de Massa Corporal) é uma fórmula usada para avaliar se uma pessoa está com o peso adequado em relação à sua altura.</p>
+                    <input
+                        type="number"
+                        placeholder="Digite a sua altura. EX: 1.5 (em metros)"
+                        className="border-b-2 border-b-gray-300 outline-none p-2 w-full mb-5"
+                        value={heightField > 0 ? heightField : ''}
+                        onChange={e => setHeightField(parseFloat(e.target.value))}
+                    />
+                    <input
+                        type="number"
+                        placeholder="Digite a seu peso. EX: 75 (em kg)"
+                        className="border-b-2 border-b-gray-300 outline-none p-2 w-full mb-5"
+                        value={weightField > 0 ? weightField : ''}
+                        onChange={e => setWeightField(parseFloat(e.target.value))}
+                    />
+                    <button onClick={handleCalculateButton} className='bg-sky-700 text-white w-full text-2xl py-3 rounded-2xl transition-all duration-[200ms] cursor-pointer mt-10 hover:opacity-70'>Calcular</button>
+                </div>
+                <div className="flex-1 ml-10">
+                    /* lado esquerdo */
+                </div>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    )
 }
+
+export default Page;
