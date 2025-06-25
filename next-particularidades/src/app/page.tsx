6f7c5cb79d533d0fff8c7922e7dedeb6f1023969
Andrey Mendonça <97072306+AndreyMendonca@users.noreply.github.com>
2025-06-25 16:14:30 -0300

    Metadados do next

--- next-particularidades/src/app/page.tsx
+++ next-particularidades/src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Página inicial'
+}
+
 const Page = () =>{
   return (
     <div className="h-screen flex items-center justify-center">
