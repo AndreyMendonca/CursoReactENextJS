1fb0b00bd97fc5223cdd51dbd79d2cb769167847
Andrey Mendonça <97072306+AndreyMendonca@users.noreply.github.com>
2025-07-07 14:53:26 -0300

    Projeto sistema de delivery

--- next-particularidades/src/components/button-hooks.tsx
+++ next-particularidades/src/components/button-hooks.tsx
@@ -9,6 +9,14 @@ export const ButtonHooks = () =>{
         router.push('/tela1')
     }
 
+    /* FUNÇÕES DO ROUTER
+    BACK - VOLTA PARA TELA ANTERIOR
+    FORWARD - AVANÇA PARA A TELA ADIANTE
+    REPLACE - VAI PARA A TELA E LIMPA O HISTORICO (USADO PARA LOGIN)
+    PREFETCH - PRE CARREGA A TELA 2, E DEPOIS A PAGINA CARREGA MAIS RAPIDO 
+    REFRESH - RECARREGAR A PAGINA
+    */
+
     return (
         <div>
             <button onClick={handleClick}>Ir para tela com push</button>
